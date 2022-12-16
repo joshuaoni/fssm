0ce72ab9ac0555e0aa41384ccd487afcba921d24
kartikeya pandey <pandeykartikeya0604@gmail.com>
2022-12-16 14:08:27 +0530

    knowledgecontent bug fixed

--- components/Sections/KnowledgeDataCont/KnowledgeDataCont.js
+++ components/Sections/KnowledgeDataCont/KnowledgeDataCont.js
@@ -113,13 +113,13 @@ const KnowledgeDataCont = () => {
                             document_type={document_type}
                             stake_holder={stake_holder}
                             geography={geography}
-                            org={organization.org_name}
+                            org={organization?.org_name}
                             urban_rural={status}
                             citation={citation}
                             language={language}
                             value_chain={value_chain}
                             description={description}
-                            theme={theme.theme_title}
+                            theme={theme?.theme_title}
                             keywords={keywords}
                           />
                         )
